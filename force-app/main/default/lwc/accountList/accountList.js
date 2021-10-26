35f6c5c49a40a864346655b3da0b301d48c0cf6d
yuko <tachibana.yuko@lobos-work.com>
2021-10-26 20:05:31 +0900

    buttonclick成功バージョン

---
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -0,0 +1,30 @@
+import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+const COLUMNS = [
+    { label: 'Account Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Annual Revenue', fieldName: REVENUE_FIELD.fieldApiName, type: 'currency' },
+    { label: 'Industry', fieldName: INDUSTRY_FIELD.fieldApiName, type: 'text' }
+];
+export default class AccountList extends LightningElement {
+    columns = COLUMNS;
+    accounts;
+    errors;
+
+    @wire(getAccounts)
+    // accounts;
+    wiredAccounts({ error, data }) {
+        
+        if (data) {
+            this.accounts = data;
+            this.error = undefined;
+            console.log('accounts: ', this.accounts);
+        } else if (error) {
+            this.errors = reduceErrors(error);
+            this.accounts = undefined;
+        }
+    }
+}
---
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+
+export default class Numerator extends LightningElement {
+    counter = 0;
+    handleIncrement() {
+      console.log('handleIncrement start');
+      this.counter++;
+    }
+    handleDecrement() {
+      this.counter--;
+    }
+    handleMultiply(event) {
+      console.log('Multiply start');
+      const factor = event.detail;
+      this.counter *= factor;
+    }
+}
